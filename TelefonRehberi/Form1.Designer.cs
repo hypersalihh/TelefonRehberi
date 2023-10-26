1302e760756d3ce77a1a4df7ef91786c9fbd6e20
Salih Becer <salihbecer38@gmail.com>
2023-10-26 07:13:16 +0300

    no message

---
+++ TelefonRehberi/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TelefonRehberi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvKisiler = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(690, 25);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 0;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(12, 222);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(115, 222);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGüncelle.TabIndex = 2;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(228, 222);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(341, 25);
+            this.txtAra.Multiline = true;
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(343, 23);
+            this.txtAra.TabIndex = 4;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(105, 25);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(101, 23);
+            this.txtAd.TabIndex = 5;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(105, 94);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(101, 22);
+            this.txtSoyad.TabIndex = 6;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(105, 161);
+            this.txtTelefon.Multiline = true;
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(101, 22);
+            this.txtTelefon.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Telefon";
+            // 
+            // dgvKisiler
+            // 
+            this.dgvKisiler.AllowUserToAddRows = false;
+            this.dgvKisiler.AllowUserToDeleteRows = false;
+            this.dgvKisiler.AllowUserToResizeColumns = false;
+            this.dgvKisiler.AllowUserToResizeRows = false;
+            this.dgvKisiler.ColumnHeadersHeight = 29;
+            this.dgvKisiler.Location = new System.Drawing.Point(341, 54);
+            this.dgvKisiler.MultiSelect = false;
+            this.dgvKisiler.Name = "dgvKisiler";
+            this.dgvKisiler.ReadOnly = true;
+            this.dgvKisiler.RowHeadersWidth = 51;
+            this.dgvKisiler.RowTemplate.Height = 24;
+            this.dgvKisiler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvKisiler.Size = new System.Drawing.Size(424, 378);
+            this.dgvKisiler.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 434);
+            this.Controls.Add(this.dgvKisiler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnAra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvKisiler;
+    }
+}
+
---
+++ TelefonRehberi/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TelefonRehberi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KisiGetir();
+        }
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable tablo;
+
+        void KisiGetir()
+        {
+            conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\hyper\\Documents\\database.mdf;Integrated Security=True;Connect Timeout=30");
+            adapter = new SqlDataAdapter("SELECT *FROM Kisiler", conn);
+            tablo = new DataTable();
+            conn.Open();
+            adapter.Fill(tablo);
+            dgvKisiler.DataSource = tablo;
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KisiGetir();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO Kisiler (Ad, Soyad, Telefon) VALUES (@ad, @soyad, @tel)";
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@ad", txtAd.Text);
+            cmd.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@tel", txtTelefon.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            KisiGetir();
+
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Update Kisiler Set ad=@Ad,soyad=@Soyad,Telefon=@tel Where Id=@id";
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@ad", txtAd.Text);
+            cmd.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@tel", txtTelefon.Text);
+            cmd.Parameters.AddWithValue("@id", dgvKisiler.CurrentRow.Cells[0].Value.ToString());
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            KisiGetir();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM Kisiler WHERE Id = @id";
+            cmd = new SqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@id", dgvKisiler.CurrentRow.Cells[0].Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            KisiGetir();
+        }
+
+        private void dgvKisiler_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtAd.Text = dgvKisiler.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dgvKisiler.CurrentRow.Cells[2].Value.ToString();
+            txtTelefon.Text = dgvKisiler.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            DataView dv = tablo.DefaultView;
+            dv.RowFilter = "Ad Like '" + txtAra.Text + "%'";
+            dgvKisiler.DataSource = dv;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+   
+        }
+    }
+}
